421fe49b51f06f279c1a7be9a8db5cf3480122fa
Sterling De Jesus <s.dejesusp@gmail.com>
2020-08-01 11:32:00 -0400

    Post with a list and a details views

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,6 +2,12 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='published')
+
+
 class Post(models.Model):
     STATUS_CHOICES = (
         ('draft', 'Draft'),
@@ -20,6 +26,8 @@ class Post(models.Model):
     status = models.CharField(max_length=10,
                             choices=STATUS_CHOICES,
                             default='draft')
+    objects = models.Manager()      # The default manager
+    published = PublishedManager()  # Our custom manager
 
     class Meta:
         ordering = ('-publish', )
