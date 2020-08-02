979cc42a57b8fc8b79a8d8e14d9cf0a3b7232dc8
Sterling De Jesus <s.dejesusp@gmail.com>
2020-08-01 22:45:22 -0400

    Post list with class-based views

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,7 +1,8 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-
+'''
 def post_list(request):
     object_list = Post.published.all()
     paginator = Paginator(object_list, 3)       # 3 posts in each page
@@ -16,7 +17,12 @@ def post_list(request):
         posts = paginator.page(paginator.num_pages)
     return render(request, 'blog/post/list.html', {'posts': posts,
                                                    'page': page })
-
+'''
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post,
